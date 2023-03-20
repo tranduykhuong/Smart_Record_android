c855d9ccb2030c75237707be3d2ef1933a8038d8
Andy Koi <anhkhoi1192002@gmail.com>
2023-03-20 17:23:31 +0700

    Update UI part 1

---
+++ app/src/main/java/com/devapp/smartrecord/ui/WaveformView.java
@@ -0,0 +1,40 @@
+package com.devapp.smartrecord.ui;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class WaveformView extends View {
+
+    Rect rect;
+    Paint paint;
+
+    public WaveformView(Context context) {
+        super(context);
+    }
+
+    public WaveformView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        rect = new Rect(0, 100, 200, 300);
+        paint = new Paint();
+        paint.setColor(Color.BLACK);
+        paint.setStrokeWidth(40);
+        paint.setStyle(Paint.Style.STROKE);
+        canvas.drawRect(rect, paint);
+    }
+}
+
+
+
+
