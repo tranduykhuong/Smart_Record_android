b8391d9826aaf5f6c90a568de822cd2da0de8027
tranduykhuong <tranduykhuongit@gmail.com>
2023-03-22 15:41:58 +0700

    DK: UI/UX settings, Alarm fragment

--- app/src/main/java/com/devapp/smartrecord/ui/alarm/ItemClassContent.java
+++ app/src/main/java/com/devapp/smartrecord/ui/alarm/ItemClassContent.java
@@ -1,2 +1,37 @@
-package com.devapp.smartrecord.ui.alarm;public class ItemClassContent {
+package com.devapp.smartrecord.ui.alarm;
+
+public class ItemClassContent {
+    private String title;
+    private String time;
+    private boolean checked;
+
+    public ItemClassContent(String title, String time, boolean checked) {
+        this.title = title;
+        this.time = time;
+        this.checked = checked;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public boolean getChecked() {
+        return checked;
+    }
+
+    public void setchecked(boolean checked) {
+        this.checked = checked;
+    }
 }
