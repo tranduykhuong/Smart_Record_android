153439058a42e70ea61525218022e0be6fce2bf5
tranduykhuong <tranduykhuongit@gmail.com>
2023-04-25 11:14:42 +0700

    DK: merge code

---
+++ graphviewlibrary/build/generated/source/buildConfig/release/suman/brose/graphviewlibrary/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package suman.brose.graphviewlibrary;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "suman.brose.graphviewlibrary";
+  public static final String BUILD_TYPE = "release";
+}
